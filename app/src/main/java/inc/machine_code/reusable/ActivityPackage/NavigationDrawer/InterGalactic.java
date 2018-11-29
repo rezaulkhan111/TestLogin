c6a7576510b59b55a42bea3aed1db10cabfc8c87
rezaulkhan111 <rezaulkhan111@hotmail.com>
2018-11-29 17:43:12 +0600

    final

--- app/src/main/java/inc/machine_code/reusable/ActivityPackage/NavigationDrawer/InterGalactic.java
+++ app/src/main/java/inc/machine_code/reusable/ActivityPackage/NavigationDrawer/InterGalactic.java
@@ -1,73 +1,52 @@
 package inc.machine_code.reusable.ActivityPackage.NavigationDrawer;
 
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import inc.machine_code.reusable.R;
 
+import static android.widget.Toast.*;
+
 public class InterGalactic extends Fragment {
 
-    private static InterGalactic myProjectListFragment;
+    //private static InterGalactic myProjectListFragment;
     private RecyclerView rv;
-    private static ArrayList<String> spacecrafts = new ArrayList<String>() {
-    };
+    private static ArrayList<String> spacecrafts = new ArrayList<String>();
 
-    private final static String TAG = "lch";
-
-    public InterGalactic getMyProjectListFragment() {
-        FragmentManager mFragmentManager = getFragmentManager();
-
-        if (mFragmentManager != null) {
-            mFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.containerID2,
-                            myProjectListFragment, "myProjectListFragment")
-                    .commit();
-        }
-        return myProjectListFragment;
-    }
+    TextView textView;
+    //  private final static String TAG = "lch";
 
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        spacecrafts.add("aaaaaaaaa");
+//        spacecrafts.add("aaaaaaaaa");
+//        spacecrafts.add("aaaaaaaaa");
+//        spacecrafts.add("aaaaaaaaa");
+//        spacecrafts.add("aaaaaaaaa");
+//        spacecrafts.add("aaaaaaaaa");
 
-        spacecrafts.add("aaaaaaaaa");
-        spacecrafts.add("aaaaaaaaa");
-        spacecrafts.add("aaaaaaaaa");
-        spacecrafts.add("aaaaaaaaa");
-        spacecrafts.add("aaaaaaaaa");
-        spacecrafts.add("aaaaaaaaa");
-
-
-        View rootView = inflater.inflate(R.layout.intergalactic, container, false);
-        rv = rootView.findViewById(R.id._recycler_view);
+        View rootView = inflater.inflate(R.layout.recycler_view_layout, container, false);
+        // rootView.findViewById(R.id.rev_layout_containerID_);
+        rv = rootView.findViewById(R.id.recycler_view_id);
 
         rv.setLayoutManager(new LinearLayoutManager(getActivity()));
-
         rv.setAdapter(new MyAdapter(getActivity(), spacecrafts));
+
         return rootView;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-
-    }
-
-    @Override
-    public String toString() {
-        return "InterGalactic";
     }
 }
--- app/src/main/java/inc/machine_code/reusable/ActivityPackage/NavigationDrawer/NavigationActivity.java
+++ app/src/main/java/inc/machine_code/reusable/ActivityPackage/NavigationDrawer/NavigationActivity.java
@@ -1,6 +1,8 @@
 package inc.machine_code.reusable.ActivityPackage.NavigationDrawer;
 
 import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,15 +13,20 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import inc.machine_code.reusable.ActivityPackage.BottomSheet.BottomSheetActivity;
 import inc.machine_code.reusable.R;
 
-public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private DrawerLayout mDrawerLayout;
-    InterGalactic obj;
+public class NavigationActivity extends AppCompatActivity {
+    DrawerLayout mDrawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @SuppressLint("CutPasteId")
     @Override
@@ -27,22 +34,16 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_drawer_activity);
 
+        initViews();
+        setToolbar();
+        setNavItemSelectedListener();
+        setDrawerLayoutListener();
+    }
+
+    private void initViews() {
         mDrawerLayout = findViewById(R.id.drawer_layout);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_action_name);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-
-        toggle.syncState();
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.nav_view);
     }
 
     @Override
@@ -55,71 +56,97 @@ public class NavigationActivity extends AppCompatActivity implements NavigationV
         }
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        // set item as selected to persist highlight
-        menuItem.setChecked(true);
-        int id = menuItem.getItemId();
-        if (id == menuItem.getItemId()) {
 
-            NavigationActivity.this.getSupportFragmentManager().beginTransaction().replace(R.id.containerID2, obj.getMyProjectListFragment()).commit();
-            mDrawerLayout.closeDrawers();
-        }
+    private void replaceFragment(android.app.Fragment fragment) {
 
+        getFragmentManager().popBackStack();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame, fragment, "HomeDairy");
+        transaction.addToBackStack(null);
+        transaction.commit();
+        Toast.makeText(this, "" + fragment.getClass(), Toast.LENGTH_SHORT).show();
 
-        // close drawer when item is tapped
-        //   mDrawerLayout.closeDrawers();
-
-        // Add code here to update the UI based on the item selected
-        // For example, swap UI fragments here
-
-
-        // DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        // drawer.closeDrawer(GravityCompat.START);
-
-        return true;
     }
 
-//        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
-//            @Override
-//            public void onDrawerSlide(@NonNull View view, float v) {
-//
-//            }
-//
-//            @Override
-//            public void onDrawerOpened(@NonNull View view) {
-//
-//            }
-//
-//            @Override
-//            public void onDrawerClosed(@NonNull View view) {
-//
-//            }
-//
-//            @Override
-//            public void onDrawerStateChanged(int i) {
-//
-//            }
-//        });
-//
-//        navigationView.setNavigationItemSelectedListener(this);
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//
-//
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        String string = (String) menuItem.getTitle();
-//        if (string == menuItem.getTitle()) {
-//
-//            startActivity(new Intent(this, BottomSheetActivity.class));
-//        }
-//        return false;
-//    }
+    public void setDrawerLayoutListener() {
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+//                        Toast.makeText(MainActivity.this, "Slide", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onDrawerOpened(@NonNull View drawerView) {
+
+                        // Respond when the drawer is opened
+                        Toast.makeText(NavigationActivity.this, "OPEN", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onDrawerClosed(@NonNull View drawerView) {
+                        // Respond when the drawer is closed
+                        Toast.makeText(NavigationActivity.this, "CLOSE", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+//                        Toast.makeText(MainActivity.this, "StateChange", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        assert actionbar != null;
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_action_name);
+    }
+
+    public void setNavItemSelectedListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                android.support.v4.app.Fragment selectedFragment = null;
+                switch (menuItem.getItemId()) {
+
+                    case R.id.item_camera:
+                        selectedFragment = new InterGalactic();
+                        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        assert selectedFragment != null;
+                        transaction.replace(R.id.frame, selectedFragment);
+                        transaction.commit();
+
+                        Log.e("aaa", "onNavigationItemSelected: "+"item_camera");
+                        break;
+
+                    case R.id.item_gallery:
+
+                        Log.e("111", "onNavigationItemSelected: ");
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
 }
